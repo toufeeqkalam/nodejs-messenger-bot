051e66823e4ddb45e6045746cfed2b6ccc492b78
tkalam <toufeeq.kalam@metropolitan.co.za>
2021-10-18 13:13:27 +0200

    added request

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const {PORT} = require('./scripts/config/config');
 const Bot = require('./scripts/bot/bot');
-const {
-    FACEBOOK_ACCESS_TOKEN,
-    FACEBOOK_VERIFY_TOKEN,
-    FACEBOOK_APP_SECRET
-} = require('./scripts/config/config');
+const CONFIG = require('./scripts/config/config');
 const app = express();
 
 const bot = new Bot({
-    token: FACEBOOK_ACCESS_TOKEN,
-    verifyToken: FACEBOOK_VERIFY_TOKEN,
-    appSecret: FACEBOOK_APP_SECRET
+    token: CONFIG.FACEBOOK_ACCESS_TOKEN,
+    verifyToken: CONFIG.FACEBOOK_VERIFY_TOKEN,
+    appSecret: CONFIG.FACEBOOK_APP_SECRET
 });
 
 app.get('/', (req, res) => {
