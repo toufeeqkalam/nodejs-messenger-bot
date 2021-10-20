e046e77d03b83f1ad90a79e15cbe9f48fb575be1
tkalam <toufeeq.kalam@metropolitan.co.za>
2021-10-20 09:50:44 +0200

    Removed config

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -1,15 +1,15 @@
 const development = {
     PORT: 5000,
-    INTERNAL_URL: 'https://api-dev.getup.metropolitan.co.za',
+    INTERNAL_URL: '',
     GRAPH_URL: 'https://graph.facebook.com/v11.0',
-    FACEBOOK_ACCESS_TOKEN: 'EAAIjpheZAu1sBAJPUE1daHMrgKeSsaKKJSCt8sU2U7BlMV8EwhNGg52mSgP2NIsQ6sxvg84TMXORFRGeQoEKJmYr1TmODNHekS6ydUxyZAptMOllOdYmDRVZA2gzf3rSaFazDQlVIDNZCuZCTtGJVuV070BthZBKtzqndKlqOtTHVUA3SYFXvwTOTmsLhuedYg2brTPbeAPgZDZD',
-    FACEBOOK_VERIFY_TOKEN: '###admin786',
-    FACEBOOK_APP_SECRET: 'd0eb8f38522033e9e993b8737cd687a4'
+    FACEBOOK_ACCESS_TOKEN: '',
+    FACEBOOK_VERIFY_TOKEN: '',
+    FACEBOOK_APP_SECRET: ''
 }
 
 const production = {
     PORT: 9080,
-    INTERNAL_URL: 'https://api.getup.metropolitan.co.za',
+    INTERNAL_URL: '',
     GRAPH_URL: 'https://graph.facebook.com/v11.0',
     FACEBOOK_ACCESS_TOKEN: '',
     FACEBOOK_VERIFY_TOKEN: '',
